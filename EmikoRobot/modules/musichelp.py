db945609c869f29ea5d719e2bfa777d232af02ca
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-10 19:22:01 +0800

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -3,17 +3,23 @@ __mod_name__ = "Musicplayer"
 __help__ = """
 ──「 Music stream 」──              
 
-❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
-❂ /playlist (choose option) for playing your personal playlist or group playlist.
+*Command for admins only.*
+• `/reload` - For refreshing the adminlist.
+• `/pause` - To pause the playback.
+• `/resume` - To resuming the playback You've paused.
+• `/skip` - To skipping the player.
+• `/end` - For end the playback.
+• `/userbotjoin` - To add @KoboAssistant to the group.
+• `/userbotleave` - To remove bots from the group.
+• `/mutemusic or /unmutemusic` - Toggle for turn ON or turn OFF the musicplayer.
 
-• Admins only
+*Command for all members.*
+• `/mplay` <query or reply audio> - Playing music via YouTube.
+• `/vplay` <query or reply video> - Playing video via YouTube.
+• `/vstream` <link steram via YouTube> - Playing video stream via YouTube.
+• `/playlist` - To playing a playlist of groups or your personal playlist
 
-❂ /auth - Add to authorized users.
-❂ /unauth - Canceling or deleting from authorized users list.
-❂ /msettings - To open panel settings on musicplayer.
-❂ /setadmin - To set all command only for admins.
-❂ /setmember - Set all command for all members.
-❂ /vol (range 1-200) - To set volume on voice calls.
+[Video-Stream](https://github.com/levina-lab/video-stream) based music player (with some changes).
 
 NOTE : DON'T SPAMMING AT REQUEST SONG OR VIDEO...
 """
