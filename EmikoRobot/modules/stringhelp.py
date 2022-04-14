838bbf21d80e553bd6be9ddc4a80958e122ba295
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-14 13:03:02 +0800

    Update stringhelp.py

--- EmikoRobot/modules/stringhelp.py
+++ EmikoRobot/modules/stringhelp.py
@@ -1,2 +1,11 @@
 __mod_name__ = "String Session"
 
+__help__ = """
+──「 String Session 」──              
+
+/help - This Message
+/start - Start the Bot
+/generate - Start Generating Session
+/cancel - Cancel the process
+/restart - Cancel the process
+"""
