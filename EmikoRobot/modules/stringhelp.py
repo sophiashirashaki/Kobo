2538baecffef7dc96b60ec012882bc6b219888e5
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-14 17:03:34 +0800

    Update stringhelp.py

--- EmikoRobot/modules/stringhelp.py
+++ EmikoRobot/modules/stringhelp.py
@@ -2,10 +2,11 @@ __mod_name__ = "String"
 
 __help__ = """
 ──「 String Session 」──              
+Kobo can generate string for you!!
 
+🔥 Available Commands 🔥
 /help - This Message
 /start - Start the Bot
 /generate - Start Generating Session
 /cancel - Cancel the process
-/restart - Cancel the process
 """
