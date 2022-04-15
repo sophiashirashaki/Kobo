1f7448382e197b6f96b0326cee2f61603af8088d
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-15 13:28:54 +0800

    Update music.py

--- EmikoRobot/modules/music.py
+++ EmikoRobot/modules/music.py
@@ -17,29 +17,6 @@ from youtubesearchpython import SearchVideos
 from EmikoRobot.utils.pluginhelper import get_text, progress
 from EmikoRobot import pbot, arq
 
-async def lyrics_func(answers, text):
-    song = await arq.lyrics(text)
-    if not song.ok:
-        answers.append(
-            InlineQueryResultArticle(
-                title="Error",
-                description=song.result,
-                input_message_content=InputTextMessageContent(
-                    song.result
-                ),
-            )
-        )
-        return answers
-    lyrics = song.result
-    song = lyrics.splitlines()
-    song_name = song[0]
-    artist = song[1]
-    if len(lyrics) > 4095:
-        lyrics = await hastebin(lyrics)
-        lyrics = f"**LYRICS_TOO_LONG:** [URL]({lyrics})"
-
-    msg = f"**__{lyrics}__**"
-
     answers.append(
         InlineQueryResultArticle(
             title=song_name,
