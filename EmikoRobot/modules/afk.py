b44e23065a6e5e25ca15f87b7b0ec97eaa013061
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-16 12:46:13 +0800

    Update afk.py

--- EmikoRobot/modules/afk.py
+++ EmikoRobot/modules/afk.py
@@ -11,7 +11,7 @@ from telegram.ext import Filters, MessageHandler, run_async
 
 from EmikoRobot import dispatcher
 from EmikoRobot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
-from EmikoRobot.modules.redis.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
+from EmikoRobot.modules.sql.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
 from EmikoRobot import REDIS
 from EmikoRobot.modules.users import get_user_id
 
