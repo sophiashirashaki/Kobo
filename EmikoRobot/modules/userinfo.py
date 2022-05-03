0238a536e0402f65e7234873af16196fd596c559
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-03 10:00:52 +0800

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -251,7 +251,7 @@ def info(update: Update, context: CallbackContext):
     else:
         return
 
-    rep = message.reply_text("<code>Sabar Oy...</code>", parse_mode=ParseMode.HTML)
+    rep = message.reply_text("<code>Wait for a sec...</code>", parse_mode=ParseMode.HTML)
 
     text = (
         f"╔═━「<b> Appraisal results:</b> 」\n"
