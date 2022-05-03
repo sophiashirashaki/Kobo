2198d14a401f1a8f2c519ed06fb5474270cbc849
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-03 11:49:39 +0800

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -558,7 +558,7 @@ Examples:
 ❂ /setbio This user is a wolf (reply to the user)
  
 *Overall Information about you:*
-❂ /info or /whois*:* get information about a user. 
+❂ /info*:* get information about a user. 
  
 *json Detailed info:*
 ❂ /json*:* Get Detailed info about any message.
