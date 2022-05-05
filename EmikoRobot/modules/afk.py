206c78fe49ba59c6e62f07c7d46cdca120ad38f2
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-05 12:19:03 +0800

    Update afk.py

--- EmikoRobot/modules/afk.py
+++ EmikoRobot/modules/afk.py
@@ -1,93 +1,88 @@
 import time
-import random
-import humanize
 
-from typing import Optional
-from datetime import datetime
-from telegram import Message, User
 from telegram import MessageEntity, ParseMode
 from telegram.error import BadRequest
-from telegram.ext import Filters, MessageHandler, run_async
+from telegram.ext import Filters
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
-from EmikoRobot.modules.sql.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
 from EmikoRobot import REDIS
-from EmikoRobot.modules.users import get_user_id
-
-from EmikoRobot.modules.helper_funcs.alternate import send_message
 from EmikoRobot.modules.helper_funcs.readable_time import get_readable_time
+from EmikoRobot.modules.helper_funcs.decorators import emikocmd, emikomsg
+from EmikoRobot.modules.sql.afk_redis import (
+    afk_reason,
+    end_afk,
+    is_user_afk,
+    start_afk,
+)
+from Natsunagi.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
 
-def afk(update, context):
-    args = update.effective_message.text.split(None, 1)
+@emikocmd(command="afk", group=AFK_GROUP)
+@emikomsg(Filters.regex("(?i)^brb"), friendly="afk", group=AFK_GROUP)
+def afk(update, _):
+    message = update.effective_message
+    args = message.text.split(None, 1)
     user = update.effective_user
+
     if not user:  # ignore channels
         return
 
-    if user.id == 777000:
+    if user.id in [777000, 1087968824]:
         return
+
     start_afk_time = time.time()
     reason = args[1] if len(args) >= 2 else "none"
-    start_afk(update.effective_user.id, reason)
-    REDIS.set(f'afk_time_{update.effective_user.id}', start_afk_time)
-    fname = update.effective_user.first_name
+    start_afk(user.id, reason)
+    REDIS.set(f"afk_time_{user.id}", start_afk_time)
+    fname = user.first_name
     try:
-        update.effective_message.reply_text(
-            "{} is now Away!".format(fname))
+        message.reply_text(f"See you later 👋 <code>{fname}</code>!", parse_mode=ParseMode.HTML)
     except BadRequest:
         pass
 
-def no_longer_afk(update, context):
+
+@emikomsg((Filters.all & Filters.chat_type.groups), friendly='afk', group=AFK_GROUP)
+def no_longer_afk(update, _):
     user = update.effective_user
     message = update.effective_message
     if not user:  # ignore channels
         return
 
-    if not is_user_afk(user.id):  #Check if user is afk or not
+    if not is_user_afk(user.id):  # Check if user is afk or not
         return
-    end_afk_time = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user.id}'))))
-    REDIS.delete(f'afk_time_{user.id}')
+
+    the_heck = REDIS.get(f"afk_time_{user.id}")
+    if not the_heck:
+        return
+
+    end_afk_time = get_readable_time((time.time() - float(the_heck)))
+    REDIS.delete(f"afk_time_{user.id}")
     res = end_afk(user.id)
     if res:
         if message.new_chat_members:  # dont say msg
             return
         firstname = update.effective_user.first_name
         try:
-            options = [
-                "{} Is wasting his time in the chat!",
-                "The Dead {} Came Back From His Grave!",
-                "Welcome back {}! I hope you bought pizza",
-                "Good to hear from you again {}",
-                "{} Good job waking up now get ready for your classes!",
-                "Hey {}! Why weren't you online for such a long time?",
-                "{} why did you came back?",
-                "{} Is now back online!",
-                "OwO, Welcome back {}",
-                "Welcome to hell again {}",
-                "Whats poppin {}?",
-            ]
-            chosen_option = random.choice(options)
-            update.effective_message.reply_text(
-                chosen_option.format(firstname),
+            message.reply_text(
+                f"Welcome back <code>{firstname}</code>!\n\nYou were away for: <code>{end_afk_time}</code>", parse_mode=ParseMode.HTML
             )
-        except BaseException:
-            pass
-            
+        except BadRequest:
+            return
 
 
-
+@emikomsg((Filters.all & Filters.chat_type.groups & ~Filters.update.edited_message), friendly="afk", group=AFK_REPLY_GROUP)
 def reply_afk(update, context):
     message = update.effective_message
     userc = update.effective_user
     userc_id = userc.id
     if message.entities and message.parse_entities(
-        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+    ):
         entities = message.parse_entities(
-            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+        )
 
         chk_users = []
         for ent in entities:
@@ -100,8 +95,9 @@ def reply_afk(update, context):
                 chk_users.append(user_id)
 
             elif ent.type == MessageEntity.MENTION:
-                user_id = get_user_id(message.text[ent.offset:ent.offset +
-                                                   ent.length])
+                user_id = get_user_id(
+                    message.text[ent.offset : ent.offset + ent.length]
+                )
                 if not user_id:
                     # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                     return
@@ -112,9 +108,12 @@ def reply_afk(update, context):
 
                 try:
                     chat = context.bot.get_chat(user_id)
-                except BadRequest:
-                    print("Error: Could not fetch userid {} for AFK module".
-                          format(user_id))
+                except BadRequest as e:
+                    print(
+                        "Error: Could not fetch userid {} for AFK module due to {}".format(
+                            user_id, e
+                        )
+                    )
                     return
                 fst_name = chat.first_name
 
@@ -129,34 +128,23 @@ def reply_afk(update, context):
         check_afk(update, context, user_id, fst_name, userc_id)
 
 
-def check_afk(update, context, user_id, fst_name, userc_id):
+def check_afk(update, _, user_id: int, fst_name: int, userc_id: int):
     if is_user_afk(user_id):
         reason = afk_reason(user_id)
-        since_afk = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user_id}'))))
+        pussy = REDIS.get(f"afk_time_{user_id}")
+        if not pussy:
+            return
+
+        since_afk = get_readable_time((time.time() - float(pussy)))
         if int(userc_id) == int(user_id):
             return
         if reason == "none":
-            res = "<b>{}</b> is Dead!\nLast Liveliness: {} Ago.".format(fst_name, since_afk)
+            res = f"<code>{fst_name}</code> is now away!\n\nLast seen: <code>{since_afk}</code>"
         else:
-            res = "{} is afk!\nReason: {}\nLast seen: {} Ago.".format(fst_name, reason, since_afk)
+            res = f"<code>{fst_name}</code> is now away!\nReason: <code>{reason}</code>\n\nLast seen: <code>{since_afk}</code>"
 
-        update.effective_message.reply_text(res)
+        update.effective_message.reply_text(res, parse_mode=ParseMode.HTML)
 
-
-def __user_info__(user_id):
-    is_afk = is_user_afk(user_id)
-    text = ""
-    if is_afk:
-        since_afk = get_readable_time((time.time() - float(REDIS.get(f'afk_time_{user_id}'))))
-        text = "This user is currently afk (away from keyboard)."
-        text += f"\nLast Seen: {since_afk} Ago."
-       
-    else:
-        text = "This user currently isn't afk (not away from keyboard)."
-    return text
-
-def __stats__():
-    return f"• {len(REDIS.keys())} Total Keys in Redis Database."
 
 def __gdpr__(user_id):
     end_afk(user_id)
