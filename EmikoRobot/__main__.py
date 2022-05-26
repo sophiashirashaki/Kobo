e129e158a4f0e39cb2c7863db969c2d4a4982246
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-26 18:27:30 +0800

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -316,7 +316,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "———『 *{}* 』———\n".format(
+                "Here is the help for the *{}* module:\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
