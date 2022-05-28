150e5cf21f3a1b387aa047b09016fc914c50f991
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-05-28 11:01:49 +0800

    Create statistics.py

---
+++ EmikoRobot/modules/statistics.py
@@ -0,0 +1,125 @@
+import time
+import requests
+import datetime
+import platform
+import EmikoRobot.modules.sql.users_sql as sql
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, __version__ as ptbver
+from telegram.constants import ParseMode
+from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler
+from telegram.helpers import escape_markdown
+
+
+from EmikoRobot import StartTime
+from EmikoRobot.__main__ import STATS
+from EmikoRobot.modules.sql import SESSION
+from EmikoRobot.modules.helper_funcs.chat_status import sudo_plus
+from psutil import cpu_percent, virtual_memory, disk_usage, boot_time
+from platform import python_version
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += f"{time_list.pop()}, "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+@sudo_plus
+async def stats(update: Update, context: CallbackContext):
+    message = update.effective_message
+    db_size = SESSION.execute(
+        "SELECT pg_size_pretty(pg_database_size(current_database()))"
+    ).scalar_one_or_none()
+    uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    botuptime = get_readable_time((time.time() - StartTime))
+    status = "*╔═━「 Kobo Statistics: 」*\n\n"
+    status += f"*➛ System Start time:* {str(uptime)}" + "\n"
+    uname = platform.uname()
+    status += f"*➛ System:* {str(uname.system)}" + "\n"
+    status += f"*➛ Node name:* {escape_markdown(str(uname.node))}" + "\n"
+    status += f"*➛ Release:* {escape_markdown(str(uname.release))}" + "\n"
+    status += f"*➛ Machine:* {escape_markdown(str(uname.machine))}" + "\n"
+
+    mem = virtual_memory()
+    cpu = cpu_percent()
+    disk = disk_usage("/")
+    status += f"*➛ CPU:* {str(cpu)}" + " %\n"
+    status += f"*➛ RAM:* {str(mem[2])}" + " %\n"
+    status += f"*➛ Storage:* {str(disk[3])}" + " %\n\n"
+    status += f"*➛ Python version:* {python_version()}" + "\n"
+    status += f"*➛ python-telegram-bot:* {str(ptbver)}" + "\n"
+    status += f"*➛ Uptime:* {str(botuptime)}" + "\n"
+    status += f"*➛ Database size:* {str(db_size)}" + "\n"
+    kb = [
+          [
+           InlineKeyboardButton("Ping", callback_data="pingCB")
+          ]
+    ]
+    try:
+        await message.reply_text(status +
+            "\n*Bot statistics*:\n"
+            + "\n".join([mod.__stats__() for mod in STATS]) +
+            "\n\n✦ [About](https://telegram.dog/erosei_1) | ✦ [Github](https://github.com/sophiashirashaki)\n\n" +
+            "╘═━「 by [Ako Update](https://t.me/projectsupdates) 」\n",
+        parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(kb), disable_web_page_preview=True, allow_sending_without_reply=True)
+    except BaseException:
+        await message.reply_text(
+            (
+                (
+                    (
+                        "\n*Bot statistics*:\n"
+                        + "\n".join(mod.__stats__() for mod in STATS)
+                    )
+                    + "\n\n✦ [About](https://telegram.dog/erosei_1) | ✦ [Github](https://github.com/sophiashirashaki)\n\n"
+                )
+                + "╘═━「 by [Ako Update](https://t.me/projectsupdates) 」\n"
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=InlineKeyboardMarkup(kb),
+            disable_web_page_preview=True,
+            allow_sending_without_reply=True,
+        )
+
+
+async def ping(update: Update, _):
+    msg = update.effective_message
+    start_time = time.time()
+    message = await msg.reply_text("Pinging...")
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    await message.edit_text(
+        "*Pong!!!*\n`{}ms`".format(ping_time), parse_mode=ParseMode.MARKDOWN
+    )
+
+
+
+async def pingCallback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    start_time = time.time()
+    requests.get("https://api.telegram.org")
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    await query.answer(f'Pong! {ping_time}ms')
+
+
+CUTIEPII_PTB.add_handler(CommandHandler("statistics", stats))
+CUTIEPII_PTB.add_handler(CallbackQueryHandler(pingCallback, pattern=r"pingCB"))
+
+
+__mod_name__ = "statistics"
